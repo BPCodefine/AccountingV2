13f64f0100fb2a0b721b473f4d516dea51623d85
Peter Baranyi <bp@codefine.com>
2025-09-02 21:17:19 +0200

    Nested menu

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -6,18 +7,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenav } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    MatMenuModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -29,13 +33,24 @@ export class AppComponent {
 
   menuItems: any[] = [
     { icon: 'home', label: 'Home', route: 'home' },
-    { icon: 'assignment', label: 'InvExpenses', route: 'InvExpenses' },
+    { icon: 'people',
+      label: 'Customers',
+      children: [
+        { icon: 'receipt_long', label: 'CustInvoices', route: 'CustomerInvoices' },
+        { icon: 'request_quote', label: 'CustAgedInvoices', route: 'CustomerAgedInvoices' },
+        { icon: 'account_balance', label: 'CustBalanceFromLedger', route: 'CustBalFromLedg'},
+        { icon: 'receipt', label: 'HulkenInvoices', route: 'HulkenInvoices' }
+      ]
+    },
+    {
+      icon: 'inventory',
+      label: 'Vendors',
+      children: [
+        { icon: 'assignment', label: 'InvExpenses', route: 'InvExpenses' },
+        { icon: 'description', label: 'VendorInvoices', route: 'VendorInvoices' },
+      ]
+    },
     { icon: 'account_balance', label: 'BankAccountLedger', route: 'BankAccountLedger' },
-    { icon: 'receipt_long', label: 'CustInvoices', route: 'CustomerInvoices' },
-    { icon: 'request_quote', label: 'CustAgedInvoices', route: 'CustomerAgedInvoices' },
-    { icon: 'account_balance', label: 'CustBalanceFromLedger', route: 'CustBalFromLedg'},
-    { icon: 'description', label: 'VendorInvoices', route: 'VendorInvoices' },
-    { icon: 'receipt', label: 'HulkenInvoices', route: 'HulkenInvoices' }
   ];
 
   close()
